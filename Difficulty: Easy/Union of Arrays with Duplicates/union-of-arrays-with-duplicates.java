0ac887366fa2eb86e73b2f66ed434f00bd528893
Venu Kumar <gudidhavenu@gmail.com>
2025-12-20 20:54:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<a.length; i++){
+            set.add(a[i]);
+        }
+        
+        for(int i=0; i<b.length; i++){
+            set.add(b[i]);
+        }
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int value : set){
+            arr.add(value);
+        }
+        
+        return arr;
+    }
+}
